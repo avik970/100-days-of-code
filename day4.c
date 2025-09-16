32488868cfa24890edde0d435811fb0b3f94dc1a
avik970 <aviksharma171@gmail.com>
2025-09-16 17:59:46 +0530

    Update day4.c

--- day4.c
+++ day4.c
@@ -1,3 +1,7 @@
+//Write a program to swap two numbers without using a third variable.
+
+
+
 #include <stdio.h>
 
 int main() {
@@ -20,4 +24,5 @@ int main() {
     printf("b = %d\n", b);
 
     return 0;
+
 }
